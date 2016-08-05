1151ba64f4a45fc3c7fb53c291a93d0a30eb1e3a
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-08-05 08:31:31 -0700

    display date and time that repo was created

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -2,7 +2,8 @@ var Lookup = require('./../js/lookup.js').lookupModule;
 
 $(document).ready(function(){
   $("#test").click(function() {
+    var user = $("#user-lookup").val();
     var currentLookup = new Lookup();
-    currentLookup.getRepos();
+    currentLookup.getRepos(user);
   });
 });
--- js/lookup.js
+++ js/lookup.js
@@ -4,18 +4,26 @@ function Lookup() {
 
 }
 
-Lookup.prototype.getRepos = function(){
-  $.get('https://api.github.com/users/baxterc/repos?access_token=' + apiKey).then(function(response){
+Lookup.prototype.getRepos = function(username){
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
     $('#results').empty();
     for (i = 0; i < response.length ; i ++)
     {
-      console.log(response[i]);
-      $('#results').append("<li><p>Project Name: " + response[i].name + "</p><p>Project Description: " + response[i].description);
+      var createDate = response[i]
+      $('#results').append("<li><p>Project Name: " + response[i].name +
+      "</p><p>Project Description: " + response[i].description +
+      "</p><p>Project Created on " + dateConverter(response[i].created_at) +
+      "<hr>");
     }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
+
+function dateConverter(convertDate) {
+  var converted = new moment(convertDate).format('MMMM Do YYYY, h:mm:ss a');
+  return converted;
+}
 
 exports.lookupModule = Lookup;
