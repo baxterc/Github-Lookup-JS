0ec6d8407286bc809995a1a2b1d338868727405c
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-08-05 08:11:12 -0700

    retrieve name and description of a user's account and display them on page as a list

--- js/lookup.js
+++ js/lookup.js
@@ -5,8 +5,14 @@ function Lookup() {
 }
 
 Lookup.prototype.getRepos = function(){
-  $.get('https://api.github.com/users/baxterc?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/baxterc/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
+    $('#results').empty();
+    for (i = 0; i < response.length ; i ++)
+    {
+      console.log(response[i]);
+      $('#results').append("<li><p>Project Name: " + response[i].name + "</p><p>Project Description: " + response[i].description);
+    }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
