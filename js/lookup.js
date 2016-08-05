1618209d87f51b6471c4b0d1280cae94cd4016fc
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-08-05 09:11:59 -0700

    display user's full name

--- js/lookup.js
+++ js/lookup.js
@@ -5,11 +5,17 @@ function Lookup() {
 }
 
 Lookup.prototype.getRepos = function(username){
+  $.get('https://api.github.com/users/' + username).then(function(usernameResponse){
+    console.log(usernameResponse);
+    $('#user-full-name').empty();
+    $("#user-full-name").html("<h3>" + usernameResponse.name + "</h3>");
+  });
   $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
     $('#results').empty();
     $('#user-image').empty();
     $('#user-image').html("<img src='" + response[0].owner.avatar_url + "'>");
+
     for (i = 0; i < response.length ; i ++)
     {
       var createDate = response[i];
